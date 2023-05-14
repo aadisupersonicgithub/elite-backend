583534233fe5e187de6830c51954a90464ee362e
DEVI PRASAD UPADHYAY <devip17072000@gmail.com>
2023-05-14 11:12:23 +0530

    upload video done

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 const cors = require('cors');
+const multer = require('multer');
+const fs = require("fs");
+require("dotenv").config();
+
+// const { testAzureConnection } = require("./server2");
+
 app.use(cors());
 
 require("dotenv").config();
@@ -65,3 +71,50 @@ app.post("/send", function (req, res) {
 });
 // to send by command
 // curl -d -url http://localhost:3001/send
+
+
+const upload = multer({ dest: 'uploads/' }); // Specify the directory to store uploaded files
+
+
+const key = process.env.KEY;
+const containerName = process.env.CONTAINER;
+// const blobServiceClient = new BlobServiceClient(key);
+// const containerClient = blobServiceClient.getContainerClient(name);
+// console.log(containerClient, blobServiceClient)
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+async function putOnAzure(file) {
+    //   const connectionString = "<your-connection-string>"; // Replace with your actual connection string
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(key);
+    //   const containerName = "<your-container-name>"; // Replace with your actual container name
+
+    const filePath = file.path ? file.path : "./power.mp4"
+    // const filePath = "./dummy.txt"
+    const fileContent = fs.readFileSync(filePath);
+    const fileName = file.originalname ? file.originalname : "power.mp4"
+
+    try {
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        const response = await containerClient.exists();
+        const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+        if (response) {
+            console.log("Azure Blob Storage connection successful!");
+            const uploadResponse = await blockBlobClient.upload(fileContent, fileContent.length);
+            console.log(uploadResponse)
+        } else {
+            console.log(`Container '${containerName}' does not exist.`);
+        }
+    } catch (error) {
+        console.error("Error testing Azure Blob Storage connection:", error);
+    }
+}
+app.post('/upload', upload.single('video'), (req, res) => {
+    // Handle the uploaded file here
+    console.log('Received file:', req.file);
+    putOnAzure(req.file);
+
+    // Perform additional processing (e.g., save the file to Azure Blob Storage)
+
+    res.send('File uploaded successfully!');
+});
